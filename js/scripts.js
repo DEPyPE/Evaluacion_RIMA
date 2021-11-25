346070c7ca8a0d107d901472b48696b44171fdb1
wamor170795 <wa.morenoramirez@gmail.com>
2021-11-25 08:00:48 -0600

    Versión final

--- js/scripts.js
+++ js/scripts.js
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', event => {
             offset: 74,
         });
     };
-
+    
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
@@ -51,9 +51,14 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+
+
+/*
     // Activate SimpleLightbox plugin for portfolio items
     new SimpleLightbox({
         elements: '#portfolio a.portfolio-box'
     });
+*/
 
 });
+
